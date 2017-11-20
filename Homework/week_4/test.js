a5fdb0c59fffba03b0e5a49c375ac3c74e5764c5
Nordin11 <nordinbouchrit@hotmail.com>
2017-11-20 13:58:35 +0100

    push html

--- Homework/week_4/test.js
+++ Homework/week_4/test.js
@@ -1,8 +1,21 @@
-function load(){
 
-	d3.xml("test.svg", "image/svg+xml", function(error, xml) {
+d3.xml("test.svg", "image/svg+xml", function(error, xml) {
+
+	// check for errors
     if (error) throw error;    
-    document.body.appendChild(xml.documentElement);    
-	});
+    document.body.appendChild(xml.documentElement);
 
-};
+    console.log(d3.select("svg"))
+    d3.select("svg")
+    	.append("rect")
+    	.attr("id", "kleur4")
+    	.attr({
+    		"x":13,
+    		"y": 138.7,
+    		"width":21,
+    		"height":29 
+    	})
+    	.attr("class", "st1")
+    	.style("fill", "#41ae76");
+});
+
