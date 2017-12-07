a4d2ba69156dd9782dd881a4495da89439237b95
Nordin11 <nordinbouchrit@hotmail.com>
2017-12-07 12:20:39 +0100

    sfgs

--- Homework/week_6_7/js/linkedviews.js
+++ Homework/week_6_7/js/linkedviews.js
@@ -3,8 +3,85 @@
 /*****************************/
 function load() {
 
+	// define margins 
+	var margin = { top: 20, right: 10, bottom: 100, left: 40 },
+		width = 700 - margin.right - margin.left,
+		height = 500 - margin.top - margin.bottom;
 
+	// define svg
+	var svg = d3.select("body")
+		.append("svg")
+		// group attributes together
+		.attr ({
+			"width": width + margin.right + margin.left,
+			"height":height + margin.top + margin.bottom
+		})
+			.append("g")
+				.attr("transform", "translate(" + margin.left + ',' + margin.right + ')');
 
+	// define the x y scales
+	var xScale = d3.scale.ordinal()
+		.rangeRoundBands([0,width], 0.2, 0.2);
+		
+	var yScale = d3.scale.linear()
+		// make sure the height goes upwards in stead of top to bottom
+		.range([height, 0]);
+
+	// define x and y axis
+	var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom");
+
+	var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left");
+
+	d3.queue()
+	.defer(d3.csv, "Werkloosheid.csv")
+	.defer(d3.csv, "Economische activiteit.csv")
+	.await(firstready)
+
+	function firstready(error, data) {
+
+		// check for errors
+		if(error) console.log("Error: data not loaded")
+
+		data.forEach(function(d) {
+			d.Werklozen = +d.Werklozen
+		});
+
+		// specify the domains of xscale yscale
+		xScale.domain(data.map(function(d) { return d.Perioden; }) );
+		yScale.domain([0, d3.max(data, function(d) { return d.Werklozen; }) ] );
+
+		// draw the bars
+		svg.selectAll("rect")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr({
+				"x": function(d) { return xScale(d.Perioden); },
+				"y": function(d) { return yScale (d.Werklozen); },
+				"width": xScale.rangeBand(),
+				"height": function(d) { return height - yScale(d.Werklozen); }
+			})
+			.attr("class", "barcolor");
+
+		// draw xAxis
+		svg.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis)
+			.style("font-size", "13px")
+			.style("fill", "#fff");
+
+		// draw yAxis
+		svg.append("g")
+			.attr("class", "y axis")
+			.call(yAxis)
+			.style("font-size", "13px")
+			.style("fill", "#fff")
+	};
 
 
 
