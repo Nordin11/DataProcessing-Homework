927900d6e59e552ca0bf7156a2ec94566e99de9b
Nordin11 <nordinbouchrit@hotmail.com>
2017-12-11 18:05:35 +0100

    esgr

--- Homework/week_6_7/js/main.js
+++ Homework/week_6_7/js/main.js
@@ -44,7 +44,7 @@ function load(){
 
 	var scatter_margin = { top: 20, right: 20, bottom: 30, left: 40 },
 					scatter_width = 880 - scatter_margin.left - scatter_margin.right,
-					scatter_height = 500 - scatter_margin.top - scatter_margin.bottom;
+					scatter_height = 700 - scatter_margin.top - scatter_margin.bottom;
 
 	// define svg for scatter
 	var scatter_svg = d3.select("#main-container")
